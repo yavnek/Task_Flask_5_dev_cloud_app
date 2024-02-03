7cdae98e0982c7c47402005c4cf0dba6aefbf69c
yavnek <jonatan.gasiorowski@gmail.com>
2024-02-03 22:06:00 +0100

    commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from flask_applicationinsights import AppInsights
 
 app = Flask(__name__)
+app.config['APPLICATIONINSIGHTS_INSTRUMENTATIONKEY'] = 'b17d2ea3-f1c6-4e58-a114-9b0b8f15a265'
+appinsights = AppInsights(app)
 
 @app.route('/')
 def index():
